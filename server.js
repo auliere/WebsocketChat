a9507c63ae7b7c0b78ba3180c7c1ff9e484faf2a
Oleg Pedorenko <olezhkoo@gmail.com>
2016-06-02 00:35:40 +0300

    Implemented smileys in websocket chat

--- server.js
+++ server.js
@@ -5,9 +5,26 @@ api.websocket = require('websocket');
 
 var index = api.fs.readFileSync('./index.html');
 
+var prefix = '/images/smileys/';
+var files = api.fs.readdirSync('.'+prefix);
+var smMap = {};
+for(element in files) {
+  smMap[':' + files[element].slice(0,-4) + ':'] = prefix + files[element];
+}
+
 var server = api.http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end(index);
+  console.log(req.url);
+  if(req.url.substring(0, prefix.length) == prefix  &&
+      req.url.indexOf('..') === -1 ) {
+        console.log('blah');
+    api.fs.readFile('.' + req.url, function(err, fileData) {
+      res.writeHead(200, {'Content-Type': 'image/png' });
+      res.end(fileData, 'binary');
+    });
+  } else {
+    res.writeHead(200);
+    res.end(index);
+  }
 });
 
 server.listen(80, function() {
@@ -25,13 +42,15 @@ ws.on('request', function(req) {
   var connection = req.accept('', req.origin);
   clients.push(connection);
   console.log('Connected ' + connection.remoteAddress);
+  connection.send(JSON.stringify({smileyMap : smMap}));
   connection.on('message', function(message) {
     var dataName = message.type + 'Data',
-        data = message[dataName];
-    console.log('Received: ' + data);
+        data = { message: message[dataName] };
+    console.log('Received: ' + data.message);
     clients.forEach(function(client) {
-      if (connection !== client) {
-        client.send(data);
+    console.log(JSON.stringify(data));
+      if (connection !== client) {        
+        client.send(JSON.stringify(data));
       }
     });
   });
